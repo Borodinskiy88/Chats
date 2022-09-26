4c1505f4ef00befd329181c454da945ca7b4c126
Бородинский Алексей <a-borodinskiy@mail.ru>
2022-09-26 18:28:04 +0300

    10

--- src/main/kotlin/ru/netology/Main.kt
+++ src/main/kotlin/ru/netology/Main.kt
@@ -5,7 +5,7 @@ class NoChatException : RuntimeException()
 fun main() {
     ChatService.addMessage(1, Message(1, "Hello", 1.03, 11.11))
     ChatService.addMessage(1, Message(2, "Hi", 2.03, 11.22))
-    ChatService.addMessage(2, Message(3, "Bye", 2.03, 11.33))
+    ChatService.addMessage(2, Message(3, "Bye", 2.04, 11.33))
     ChatService.printChats()
     println(ChatService.getMessages(1, 1))
     ChatService.printChats()
